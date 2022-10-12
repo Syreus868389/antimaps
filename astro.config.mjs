dc28b9011b2f539a8cd31fde48ca1b46b0d139f3
Théo Pavlovic <pavlovic.theo@gmail.com>
2022-10-12 02:55:28 +0200

    cleaning

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
-import alpinejs from "@astrojs/alpinejs";
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [tailwind(), react(), alpinejs(), svelte()]
+  integrations: [tailwind(), react()]
 });
